6847cbe2ebc664ffc884f5daaaa4e076f8b2bb50
CLEITON DA SILVA BATISTA <232.976493@universidadesenaicimatec.edu.br>
2026-05-25 09:58:50 -0300

    Corrigindo banco PostgreSQL Railway

--- Program.cs
+++ Program.cs
@@ -34,14 +34,22 @@ builder.Services.AddSwaggerGen(c =>
     });
 });
 
-var connectionString =
-    Environment.GetEnvironmentVariable("DATABASE_URL")
-    ?? builder.Configuration.GetConnectionString("DefaultConnection");
+static string ConvertDatabaseUrlToConnectionString(string databaseUrl)
+{
+    var uri = new Uri(databaseUrl);
+    var userInfo = uri.UserInfo.Split(':');
+
+    return $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
+}
+
+var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+var connectionString = !string.IsNullOrEmpty(databaseUrl)
+    ? ConvertDatabaseUrlToConnectionString(databaseUrl)
+    : builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString));
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 builder.Services.AddScoped<IContaRepository, ContaRepository>();
 builder.Services.AddScoped<ITransacaoRepository, TransacaoRepository>();
